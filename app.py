696e91705387c5903a81ee523290277e9070aebe
Richardson <richardm@ucl.br>
2021-10-17 17:55:50 -0300

    Layout index e login

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def login():  # put application's code here
+    return render_template("login.html")
+
+
+@app.route('/index.html')
+def index():  # put application's code here
+    i = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+    return render_template("index.html", i=i)
 
 
 if __name__ == '__main__':
