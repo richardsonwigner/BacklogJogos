c8c4de1e2a5527ba33997e88665c3cd67ea9893f
Richardson <richardm@ucl.br>
2021-10-18 08:39:41 -0300

    Implementando a Api

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__)
 
+apikey = 'f3463a88067a455892363f8ead0bd700'
+request = requests.get(f'https://api.rawg.io/api/platforms?key={apikey}')
 
+
+for i in range(50):
+    datajson = request.json()['results'][i]['games']
+    for item in datajson:
+        i += 1
+        print(item['name'])
 @app.route('/')
 def login():  # put application's code here
+    apikey = 'f3463a88067a455892363f8ead0bd700'
+    request = requests.get(f'https://api.rawg.io/api/platforms?key={apikey}')
+    datajson = request.json()['results'][1]
+    print(datajson)
     return render_template("login.html")
 
 
