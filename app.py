b9cc718306edb8f0455d2108cf71c99d6f451f6f
Richardson <richardm@ucl.br>
2021-10-18 10:58:04 -0300

    Busca na api

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
+import json
+
 from flask import Flask, render_template
 import requests
 app = Flask(__name__)
 
 apikey = 'f3463a88067a455892363f8ead0bd700'
-request = requests.get(f'https://api.rawg.io/api/platforms?key={apikey}')
 
 
-for i in range(50):
-    datajson = request.json()['results'][i]['games']
-    for item in datajson:
-        i += 1
-        print(item['name'])
+gameName = 'Mega Man 6'
+search = f'&search={gameName}'
+url = f'https://api.rawg.io/api/games?key={apikey}{search}'
+request = requests.get(url)
+data = request.json()
+print(data['results'][0]['background_image'])
+
+# for i in range(50):
+#     datajson = request.json()['results'][i]['games']
+#    for item in datajson:
+#       i += 1
+#        print(item['name'])
+
+
+
 @app.route('/')
 def login():  # put application's code here
     apikey = 'f3463a88067a455892363f8ead0bd700'
