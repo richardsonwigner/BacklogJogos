63267977f8539342a7650ea422f3ea890c9b090a
FrancisJr <francisdaluz@ucl.br>
2021-10-20 17:35:55 -0300

    Acrescentando telas adicionar.html e editar.html

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import json
 from Classes import User
 from flask import Flask, render_template, request, redirect, session, flash
 import requests
+
 app = Flask(__name__)
 
 apikey = 'f3463a88067a455892363f8ead0bd700'
 app.secret_key = 'secretKey'
-
 
 # gameName = 'Mega Man 6'
 # search = f'&search={gameName}'
@@ -18,26 +18,28 @@ app.secret_key = 'secretKey'
 url = f'https://api.rawg.io/api/games?key={apikey}'
 requestapi = requests.get(url)
 
-
 user1 = User.User('1', 'user1', '123')
 user2 = User.User('2', 'user2', '123')
 
 ListUsers = [user1, user2]
 
+
 @app.route('/')
 def login():  # put application's code here
     return render_template("login.html")
+
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
     user_name = request.form['user']
     password_user = request.form['password']
     for user in ListUsers:
-          if user.nome == user_name and user.senha == password_user:
-              return  redirect('/index')
-          else:
-              flash('Erro no login')
-              return redirect('/')
+        if user.nome == user_name and user.senha == password_user:
+            return redirect('/index')
+        else:
+            flash('Erro no login')
+            return redirect('/')
+
 
 @app.route('/index')
 def index():  # put application's code here
@@ -46,12 +48,22 @@ def index():  # put application's code here
     # url = f'https://api.rawg.io/api/games?key={apikey}{search}'
     # requestapi = requests.get(url)
     # data = requestapi.json()
-    #imagemjogo = data['results'][0]['background_image']
+    # imagemjogo = data['results'][0]['background_image']
 
-    #for i in range(10):
+    # for i in range(10):
     datajson = requestapi.json()['results']
     number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
     return render_template("index.html", i=number, datajson=datajson)
+
+
+@app.route('/editar.html')
+def telaEditar():  # put application's code here
+    return render_template("editar.html")
+
+
+@app.route('/adicionar.html')
+def telaAdicionar():  # put application's code here
+    return render_template("adicionar.html")
 
 
 if __name__ == '__main__':
