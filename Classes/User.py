ae9bdd789e17b00ab121841c24c34f964acdaf47
Richardson <richardm@ucl.br>
2021-10-20 10:44:27 -0300

    Criando usuários e login

---
+++ Classes/User.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import json
-
-from flask import Flask, render_template
+from Classes import User
+from flask import Flask, render_template, request, redirect, session, flash
 import requests
 app = Flask(__name__)
 
 apikey = 'f3463a88067a455892363f8ead0bd700'
+app.secret_key = 'secretKey'
 
 
-gameName = 'Mega Man 6'
-search = f'&search={gameName}'
-url = f'https://api.rawg.io/api/games?key={apikey}{search}'
-request = requests.get(url)
-data = request.json()
-print(data['results'][0]['background_image'])
+# gameName = 'Mega Man 6'
+# search = f'&search={gameName}'
+# url = f'https://api.rawg.io/api/games?key={apikey}{search}'
+# request = requests.get(url)
+# data = request.json()
+# print(data['results'][0]['background_image'])
 
 # for i in range(50):
 #     datajson = request.json()['results'][i]['games']
@@ -21,17 +22,32 @@ print(data['results'][0]['background_image'])
 #        print(item['name'])
 
 
+user1 = User.User('1', 'user1', '123')
+user2 = User.User('2', 'user2', '123')
+
+ListUsers = [user1, user2]
 
 @app.route('/')
 def login():  # put application's code here
-    apikey = 'f3463a88067a455892363f8ead0bd700'
-    request = requests.get(f'https://api.rawg.io/api/platforms?key={apikey}')
-    datajson = request.json()['results'][1]
-    print(datajson)
     return render_template("login.html")
 
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    user_name = request.form['user']
+    password_user = request.form['password']
+    for user in ListUsers:
+          if user.nome == user_name and user.senha == password_user:
+              return  redirect('/index')
+          else:
+              flash('Erro no login')
+              return redirect('/')
 
-@app.route('/index.html')
+    # if user in usuarios:
+    #     if usuarios[user].senha == password_user:
+    #         session['usuario_logado'] = request.form['user']
+    #         return redirect('/index')
+
+@app.route('/index')
 def index():  # put application's code here
     i = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
     return render_template("index.html", i=i)
